58a22da46299335566f9d397848e377749638e42
Pavel <59092937+Pavel-Gorokhovskii@users.noreply.github.com>
2020-04-21 19:52:53 +0300

    Update index2.php

--- hw/index2.php
+++ hw/index2.php
@@ -7,3 +7,15 @@ $b = new B();
 echo $b
     ->setInnerText('Hello World')
     ->html();
+
+    $i = new I();
+
+echo $i
+    ->setInnerText('Hello World')
+    ->html();
+
+    $u = new U();
+
+echo $u
+    ->setInnerText('Hello World')
+    ->html();
